314402cad7486fc57ca0f08bada5be746077e3d7
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-09-19 19:26:26 +0300

    Update human-traffic-of-stadium.sql

--- MySQL/human-traffic-of-stadium.sql
+++ MySQL/human-traffic-of-stadium.sql
@@ -1,17 +1,17 @@
-# Time:  O(n^3)
-# Space: O(n^3)
+# Time:  O(n)
 
-SELECT DISTINCT s1.*
-FROM stadium AS s1, stadium AS s2, stadium AS s3
-WHERE s1.people >= 100 AND s2.people >= 100 AND s3.people >= 100
-AND
-(
-    (s2.id = s1.id + 1 AND s3.id = s2.id + 1 AND s3.id = s1.id + 2) -- s1, s2, s3
-    OR
-    (s1.id = s2.id + 1 AND s3.id = s1.id + 1 AND s3.id = s2.id + 2) -- s2, s1, s3
-    OR
-    (s3.id = s2.id + 1 AND s1.id = s3.id + 1 AND s1.id = s2.id + 2) -- s2, s3, s1
+# Write your MySQL query statement below
+
+WITH MoreThan100People AS (
+    SELECT *, id - ROW_NUMBER() OVER (ORDER BY id) AS groupID
+    FROM Stadium
+    WHERE people >= 100
 )
-ORDER BY s1.id
-;
-
+SELECT id, visit_date, people
+FROM MoreThan100People 
+WHERE groupID IN (
+    SELECT groupID
+    FROM MoreThan100People
+    GROUP BY groupID
+    HAVING COUNT(*) >= 3
+)
